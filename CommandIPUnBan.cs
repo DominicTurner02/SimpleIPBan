b994afb53f6c098d63f4a0f9d9edcbfcce57a148
MrKwabs <40438275+MrKwabs@users.noreply.github.com>
2018-12-23 21:46:08 +0000

    Added /IPUnban

---
+++ CommandIPUnBan.cs
@@ -0,0 +1,38 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+namespace SimpleIPBan
+{
+    class CommandIPUnban : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+        public string Name => "IPUnban";
+        public string Help => "Unbans an IP.";
+        public string Syntax => "<IP>";
+        public List<string> Aliases => new List<string>();
+        public List<string> Permissions => new List<string>() { "ipban.ipunban" };
+
+
+        public void Execute(IRocketPlayer rCaller, params string[] Command)
+        {
+            if (Command.Length > 0)
+            {
+                UnturnedChat.Say(rCaller, $"/IPUnban {Syntax}", Color.red);
+                return;
+            }
+
+            if (SimpleIPBan.Instance.BlacklistedIPs.Contains(Command[0]))
+            {
+                SimpleIPBan.Instance.RemoveIP(rCaller, Command[0]);
+
+
+            }
+
+
+        }
+    }
+}
